8db5492b74f7846e1dce9419a74b56aa8030ba0e
deft <deft@m-e-g-a.org>
2022-01-08 01:49:00 +0100

    bumped ROM version to 920287

--- gen.py
+++ gen.py
@@ -487,7 +487,7 @@ addline('play b$,a$,m1$,,,m2$:sleep 0.05:play ,,,b$,a$')
 
 # PRETTY LOOP
 addline('sc=0:pk=16:s$="press any key to begin!')
-addline('cursor 29,19:color 11:print "rom 920285 - pal mode";')
+addline('cursor 29,19:color 11:print "rom 920287 - pal mode";')
 addlabel('.tloop')
 addline('gosub .drawborder')
 addline('x=28:y=21:pk=pk+1:gosub .rainbowstr')
